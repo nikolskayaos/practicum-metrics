0488116666a81fa7d107467264334d3ba5c5d46b
Никольская Ольга <nikolskaya@tutu.tech>
2023-07-18 11:04:13 +0300

    инкремент 2

---
+++ cmd/agent/client.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"path"
+	"time"
+)
+
+const BaseURL = "http://localhost:8080/update"
+
+type Client struct {
+	client http.Client
+}
+
+func NewClient() *Client {
+	return &Client{
+		client: http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+func (cl *Client) SendGaugeMetric(name string, value float64) {
+	url := path.Join(BaseURL, "gauge", name, fmt.Sprintf("%f", value))
+	request, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	request.Header.Set("Content-Type", "text/plain ")
+	response, err := cl.client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+	_, err = io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func (cl *Client) SendCounterMetric(name string, value int64) {
+	url := path.Join(BaseURL, "counter", name, fmt.Sprintf("%d", value))
+	request, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	request.Header.Set("Content-Type", "text/plain ")
+	response, err := cl.client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer response.Body.Close()
+	_, err = io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,68 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"time"
+)
+
+func main() {
+	var m runtime.MemStats
+	var pollCount int64
+	var randomValue float64
+
+	reportInterval := 10 * time.Second
+	pollInterval := 2 * time.Second
+
+	client := NewClient()
+
+	go func() {
+		for {
+			runtime.ReadMemStats(&m)
+			pollCount++
+			randomValue = rand.ExpFloat64()
+			fmt.Println(pollCount, randomValue)
+			time.Sleep(pollInterval)
+		}
+	}()
+
+	for {
+		alloc := m.Alloc
+		buckHashSys := m.BuckHashSys
+
+		client.SendGaugeMetric("Alloc", float64(m.Alloc))
+		client.SendGaugeMetric("BuckHashSys", float64(m.BuckHashSys))
+		client.SendGaugeMetric("GCCPUFraction", m.GCCPUFraction)
+		client.SendGaugeMetric("GCSys", float64(m.GCSys))
+		client.SendGaugeMetric("HeapAlloc", float64(m.HeapAlloc))
+		client.SendGaugeMetric("HeapIdle", float64(m.HeapIdle))
+		client.SendGaugeMetric("HeapInuse", float64(m.HeapInuse))
+		client.SendGaugeMetric("HeapObjects", float64(m.HeapObjects))
+		client.SendGaugeMetric("HeapReleased", float64(m.HeapReleased))
+		client.SendGaugeMetric("HeapSys", float64(m.HeapSys))
+		client.SendGaugeMetric("LastGC", float64(m.LastGC))
+		client.SendGaugeMetric("Lookups", float64(m.Lookups))
+		client.SendGaugeMetric("MCacheInuse", float64(m.MCacheInuse))
+		client.SendGaugeMetric("MCacheSys", float64(m.MCacheSys))
+		client.SendGaugeMetric("MSpanInuse", float64(m.MSpanInuse))
+		client.SendGaugeMetric("MSpanSys", float64(m.MSpanSys))
+		client.SendGaugeMetric("Mallocs", float64(m.Mallocs))
+		client.SendGaugeMetric("NextGC", float64(m.NextGC))
+		client.SendGaugeMetric("NumForcedGC", float64(m.NumForcedGC))
+		client.SendGaugeMetric("NumGC", float64(m.NumGC))
+		client.SendGaugeMetric("OtherSys", float64(m.OtherSys))
+		client.SendGaugeMetric("PauseTotalNs", float64(m.PauseTotalNs))
+		client.SendGaugeMetric("StackInuse", float64(m.StackInuse))
+		client.SendGaugeMetric("StackSys", float64(m.StackSys))
+		client.SendGaugeMetric("Sys", float64(m.Sys))
+		client.SendGaugeMetric("TotalAlloc", float64(m.TotalAlloc))
+		client.SendGaugeMetric("TotalAlloc", float64(m.TotalAlloc))
+		client.SendGaugeMetric("RandomValue", randomValue)
+
+		client.SendCounterMetric("PollCount", pollCount)
+		fmt.Println("alloc", alloc)
+		fmt.Println("buckHashSys", buckHashSys)
+		time.Sleep(reportInterval)
+	}
+}
